a0a2d61cc4e8603fefd86b854a1e1a4204b735bd
Khadija <1011hadija@gmail.com>
2024-12-26 05:37:03 -0800

    Add files via upload

---
+++ onee/script.js
@@ -0,0 +1,6 @@
+let x = 3.14;
+let y = 3;
+document.getElementById("demo").innerHTML = x + "<br>" + y;
+let xx = 123e5;
+let yy = 123e-5;
+document.getElementById("demon").innerHTML = xx + "<br>" + yy;
